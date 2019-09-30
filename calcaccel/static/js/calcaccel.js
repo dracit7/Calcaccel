166eefe7e611f72cc256e6eec20efc6b364527ea
dracit7 <lychzhz@gmail.com>
2019-09-30 15:30:41 +0800

    Improved the game logic, added score board and difficulty ranking.
    - A score board on game page, every correct answer increases the score.
    - For each range of score, there will be a difficulty rank, which would influence quizes, timing and so on.

--- calcaccel/static/js/calcaccel.js
+++ calcaccel/static/js/calcaccel.js
@@ -1,12 +1,41 @@
 
+/*
+ * Helper functions
+ */
+
+function getTime() {
+  return parseInt($('#timer').attr("value"), 10);
+}
+
+function getScore() {
+  return parseInt($('#score').text(), 10);
+}
+
+function getRank() {
+  if (getScore() < 30) {
+    return 1;
+  } else if (getScore() < 80) {
+    return 2;
+  } else if (getScore() < 150) {
+    return 3
+  } else {
+    return 4;
+  }
+}
+
+/*
+ * Main functions
+ */
+
 // Call this function one time per second to implement a timer.
+// Or, call it to punish someone.
 function sub1s() {
 
   var timenow = $('#timer').attr("value"); // use a int to store the time rest.
   var width = parseFloat(timenow) * 100 / 120; // Parse the time to a percentage.
   
   // If there's no time left, game over
-  if (parseInt(timenow, 10) == 0) {
+  if (parseInt(timenow, 10) <= 0) {
     alert('Game over!');
     window.location.assign('/survival');
   }
@@ -16,8 +45,99 @@ function sub1s() {
   $('#timer').attr("value", (parseInt(timenow, 10)-1).toString());
   $('#time').text((parseInt(timenow, 10)).toString()+"s");
 
+  checkprogress();
+
+  $('#time-control').text("");
+
 }
 
+// Call this function to increase the value of timer by x.
+function plusxs(x) {
+
+  var timenow = $('#timer').attr("value"); // use a int to store the time rest.
+  var width = parseFloat(timenow) * 100 / 120; // Parse the time to a percentage.
+  
+  // Update the timer progress bar
+  $('#timer').css("width", width.toString()+"%");
+  $('#timer').attr("value", (parseInt(timenow, 10)+x).toString());
+  $('#time').text((parseInt(timenow, 10)).toString()+"s");
+
+  checkprogress();
+
+  $('#time-control').text("+"+x.toString()+"s");
+
+}
+
+// If time left exceeds 120s, then change the outfit of the progress bar.
+function checkprogress() {
+
+  var timenow = $('#timer').attr("value"); // use a int to store the time rest.
+
+  if (parseInt(timenow, 10) < 10) {
+
+    if ($('#timer').hasClass('bg-success')) {
+      $('#timer').removeClass('bg-success');
+      $('#timer').addClass('bg-danger');
+    } else if ($('#timer').hasClass('bg-info')) {
+      $('#timer').removeClass('bg-info');
+      $('#timer').addClass('bg-danger');
+    } else if ($('#timer').hasClass('bg-primary')) {
+      $('#timer').removeClass('bg-primary');
+      $('#timer').addClass('bg-danger');
+    }
+
+    $('#time').css("font-size", "180%");
+
+  } else if (parseInt(timenow, 10) < 60) {
+
+    if ($('#timer').hasClass('bg-danger')) {
+      $('#timer').removeClass('bg-danger');
+      $('#timer').addClass('bg-success');
+    } else if ($('#timer').hasClass('bg-info')) {
+      $('#timer').removeClass('bg-info');
+      $('#timer').addClass('bg-success');
+    } else if ($('#timer').hasClass('bg-primary')) {
+      $('#timer').removeClass('bg-primary');
+      $('#timer').addClass('bg-success');
+    }
+
+    $('#time').css("font-size", "180%");
+
+  } else if (parseInt(timenow, 10) < 120) {
+
+    if ($('#timer').hasClass('bg-danger')) {
+      $('#timer').removeClass('bg-danger');
+      $('#timer').addClass('bg-info');
+    } else if ($('#timer').hasClass('bg-success')) {
+      $('#timer').removeClass('bg-success');
+      $('#timer').addClass('bg-info');
+    } else if ($('#timer').hasClass('bg-primary')) {
+      $('#timer').removeClass('bg-primary');
+      $('#timer').addClass('bg-info');
+    }
+
+    $('#time').css("font-size", "180%");
+
+  } else {
+
+    if ($('#timer').hasClass('bg-danger')) {
+      $('#timer').removeClass('bg-danger');
+      $('#timer').addClass('bg-primary');
+    } else if ($('#timer').hasClass('bg-success')) {
+      $('#timer').removeClass('bg-success');
+      $('#timer').addClass('bg-primary');
+    } else if ($('#timer').hasClass('bg-info')) {
+      $('#timer').removeClass('bg-info');
+      $('#timer').addClass('bg-primary');
+    }
+
+    $('#time').css("font-size", "300%");
+
+  }
+
+}
+
+// return a ope b
 function calculate(a, b, ope) {
 
   switch (ope) {
@@ -33,13 +153,142 @@ function calculate(a, b, ope) {
 
 }
 
+// Rank 1:
+//   +-*/, 0 < operand <= 10, add 5s per correction, sub 1s per fault
+// Rank 2:
+//   +-*/, -10 < operand <= 10, add 5s per correction, sub 2s per fault
+// Rank 3:
+//   +-*/, -10 < operand <= 10, add 3s per correction, sub 3s per fault
+// Rank 4:
+//   +-*/, -20 < operand <= 20, add 3s per correction, sub 4s per fault
+function init_quiz(rank) {
+
+  var ope_l;
+  var ope_r;
+  var ope;
+
+  switch (rank) {
+    case 1:
+      ope_l = Math.floor(Math.random() * 10) + 1;
+      ope_r = Math.floor(Math.random() * ope_l) + 1;
+      switch (Math.floor(Math.random() * 4)) {
+        case 0:
+          ope = '+';
+          break;
+        case 1:
+          ope = '-';
+          break;
+        case 2:
+          ope = '*';
+          break;
+        case 3:
+          ope = '/';
+          ope_l = Math.floor(Math.random() * 10 + 1) * ope_r;
+          break;
+      }
+      $('#operand-l').text(ope_l.toString());
+      $('#operand-r').text(ope_r.toString());
+      $('#operator').text(ope);
+      break;
+    case 2:
+    case 3:
+      ope_l = Math.floor(Math.random() * 20) - 10;
+      ope_r = Math.floor(Math.random() * 20) - 10;
+      if (ope_r == 0) {
+        ope_r++;
+      }
+      switch (Math.floor(Math.random() * 4)) {
+        case 0:
+          ope = '+';
+          break;
+        case 1:
+          ope = '-';
+          break;
+        case 2:
+          ope = '*';
+          break;
+        case 3:
+          ope = '/';
+          ope_l = Math.floor(Math.random() * 10 + 1) * ope_r;
+          break;
+      }
+      $('#operand-l').text(ope_l.toString());
+      $('#operand-r').text(ope_r.toString());
+      $('#operator').text(ope);
+      break;
+    case 4:
+      ope_l = Math.floor(Math.random() * 40) - 20;
+      ope_r = Math.floor(Math.random() * 40) - 20;
+      switch (Math.floor(Math.random() * 4)) {
+        case 0:
+          ope = '+';
+          break;
+        case 1:
+          ope = '-';
+          break;
+        case 2:
+          ope = '*';
+          break;
+        case 3:
+          ope = '/';
+          ope_l = Math.floor(Math.random() * 20 - 10) * ope_r;
+          if (ope_l == 0) {
+            ope_l = -1;
+          }
+          break;
+      }
+      $('#operand-l').text(ope_l.toString());
+      $('#operand-r').text(ope_r.toString());
+      $('#operator').text(ope);
+      break;
+ 
+  }
+
+  $('#operand-l').text()
+
+}
+
+// Increase the score by `credit` times.
+function incscore(credit) {
+
+  var score = getScore();
+
+  // If we're in ACCEL TIME, double the credit.
+  if (getTime() > 120) {
+    $('#score').text((score + 2 * credit).toString());
+  } else {
+    $('#score').text((score + credit).toString());
+  }
+
+  // Animation
+  anime.timeline()
+  .add({
+    targets: '#score',
+    opacity: [0,1],
+    fontSize: ['150%', '100%'],
+    easing: "easeInOutCirc",
+    duration: 500,
+    delay: 0,
+  });
+
+}
+
+// Catch a keyboard input a handle it.
 function getKeyboardInput(eve) {
 
   var keynum = window.event ? eve.keyCode : eve.which;
 
+  const Toast = Swal.mixin({
+    toast: true,
+    showConfirmButton: false,
+    animation: false,
+    timer: 500
+  })
+
   /*
    * Enter:     13
    * 0-9:       48-57
+   * -:         189
    * Backspace: 8
    */
   switch (keynum) {
@@ -50,14 +299,44 @@ function getKeyboardInput(eve) {
         $('#operator').text()
       );
       if (result == parseInt($('#input').text(), 10)) {
-        alert('right!');
+
+        // If answered right, increase time 
+        switch (getRank()) {
+          case 1: case 2:
+            plusxs(5);
+            break;
+          case 3: case 4:
+            plusxs(3);
+            break;
+        }
+        
+        // Clear input buffer
+        $("#input").text("");
+        
+        // Score
+        incscore(1);
+
       } else {
-        alert('wrong!');
+
+        // Elsewise, decrease time
+        for (let i = 0; i < getRank(); i++) {
+          sub1s();
+        }
+        $("#input").text("");
+        $('#time-control').text("-"+getRank().toString()+"s");
+
       }
+
+      init_quiz(getRank());
+
       break;
+
     case 8:
       var num = $("#input").text();
       $("#input").text(num.substring(0, num.length - 1));
+      break;
+    case 189:
+      $('#input').text($('#input').text() + "-");
       break;
     case 48:
       $('#input').text($('#input').text() + "0");
