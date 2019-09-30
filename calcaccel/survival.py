d88b9e9d812cb0aa3da687f373d6d63e0651d1d3
MrConnorKenway <auriaiur@outlook.com>
2019-09-30 17:03:54 +0800

    add a function to update score - modify gitignore

--- calcaccel/survival.py
+++ calcaccel/survival.py
@@ -16,37 +16,47 @@ from calcaccel.auth import login_required
 
 bp = Blueprint("survival", __name__, url_prefix="/survival")
 
+
 @bp.route("/")
 @login_required
 def index():
-  """Index page of survival mode."""
-  db = get_db()
+    """Index page of survival mode."""
+    db = get_db()
 
-  you = db.execute(
-    "SELECT * FROM user"
-    " WHERE username = ?",
-    (session["user_id"],)
-  ).fetchone()
+    you = db.execute(
+        "SELECT * FROM user"
+        " WHERE id = ?",
+        (session["user_id"],)
+    ).fetchone()
 
-  users = db.execute(
-    "SELECT username, maxgrade FROM user"
-    " WHERE identity = ?"
-    " ORDER BY maxgrade DESC",
-    ("child",)
-  ).fetchall()
+    users = db.execute(
+        "SELECT username, maxgrade FROM user"
+        " WHERE identity = ?"
+        " ORDER BY maxgrade DESC",
+        ("child",)
+    ).fetchall()
 
-  return render_template("survival/index.html", users=users, you=you)
+    return render_template("survival/index.html", users=users, you=you)
+
 
 @bp.route("/play")
 @login_required
 def play():
-  """Play page"""
-  return render_template("survival/play.html")
+    """Play page"""
+    return render_template("survival/play.html")
+
 
 @bp.route("/score", methods=["POST"])
 @login_required
 def score():
-  """Send score back to backend"""
-  db = get_db()
+    """Send score back to backend"""
+    db = get_db()
 
-  # TODO: 在这里插入将用户的成绩记录到数据库中的代码
+    score = request.form["score"]
+    user_id = session["user_id"]
+    maxgrade = db.execute(
+        "SELECT maxgrade FROM user WHERE id = ?", (user_id)).fetchone()
+    if maxgrade < score:
+        db.execute("UPDATE user SET maxgrade = ? WHERE username = ?",
+                   (score, user_id))
+        db.commit()
