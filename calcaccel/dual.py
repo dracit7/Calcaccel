82a0e829c1abaaa4452eff644c37c69477c3ec2e
MrConnorKenway <auriaiur@outlook.com>
2019-10-07 10:46:39 +0800

    add synchronization feature in backend

--- calcaccel/dual.py
+++ calcaccel/dual.py
@@ -16,72 +16,77 @@ bp = Blueprint("dual", __name__, url_prefix="/dual")
 @bp.route("/", methods=["POST", "GET"])
 @login_required
 def index():
-	"""Index page of dual mode."""
-	you = query(
-			"SELECT * FROM user"
-			" WHERE id = ?",
-			(session["user_id"],),
-			fetchone=True
-	)
+    """Index page of dual mode."""
+    you = query(
+        "SELECT * FROM user"
+        " WHERE id = ?",
+        (session["user_id"],),
+        fetchone=True
+    )
 
-	if request.method == "POST":
-		peer = query("SELECT * FROM user WHERE username = ?",
-								 (request.form["username"],), fetchone=True)
+    if request.method == "POST":
+        peer = query("SELECT * FROM user WHERE username = ?",
+                     (request.form["username"],), fetchone=True)
 
-		error = None
+        error = None
 
-		if peer is None:
-			error = "No such user."
-			flash(error, "error")
+        if peer is None:
+            error = "No such user."
+            flash(error, "error")
 
-		if peer == you:
-			error = "You cannot play with yourself!"
-			flash(error, "error")
+        if peer == you:
+            error = "You cannot play with yourself!"
+            flash(error, "error")
 
-		if error is None:
-			session["peer_id"] = peer["id"]
-			execute("INSERT INTO runtime_data (id, score, timeleft, currenttime) VALUES (?, ?, ?, ?)",
-							(you["id"], 0, 0, int(time.time())))
-			return redirect("/dual/play?peer=%s" % peer["username"])
+        if error is None:
+            session["peer_id"] = peer["id"]
+            execute("INSERT INTO runtime_data (id, score, timeleft, currenttime) VALUES (?, ?, ?, ?)",
+                    (you["id"], 0, 0, int(time.time())))
+            while True:
+                time.sleep(1)
+                runtime = query(
+                    "SELECT * FROM runtime_data WHERE id = ?", (peer["id"],), fetchone=True)
+                if runtime is not None:
+                    return redirect("/dual/play?peer=%s" % peer["username"])
 
-	return render_template("dual/index.html")
+    return render_template("dual/index.html")
 
 
 @bp.route("/play", methods=["POST", "GET"])
 @login_required
 def play():
-	"""Play page"""
-	return render_template("dual/play.html", peer=request.args["peer"])
+    """Play page"""
+    return render_template("dual/play.html", peer=request.args["peer"])
 
 
 @bp.route("/message", methods=["POST", "GET"])
 @login_required
 def message():
-	if request.method == "POST":
-		score = request.form["score"]
-		timeleft = request.form["timeleft"]
-		execute(
-			"UPDATE runtime_data SET (score, timeleft, currenttime) = (?, ?, ?) WHERE id = ?",
-			(score, timeleft, int(time.time()), session["user_id"])
-		)
+    if request.method == "POST":
+        score = request.form["score"]
+        timeleft = request.form["timeleft"]
+        execute(
+            "UPDATE runtime_data SET (score, timeleft, currenttime) = (?, ?, ?) WHERE id = ?",
+            (score, timeleft, int(time.time()), session["user_id"])
+        )
 
-	if request.method == "GET":
-		peer = query(
-			"SELECT * FROM runtime_data WHERE id = ?",
-			(session["peer_id"],),
-			fetchone=True
-		)
-		if peer == None:
-			return ""
-		_, score, timeleft, currenttime = peer
-		request.form["score"] = score
-		if currenttime + 2 < int(time.time()):
-			# TODO: config time interval
-			request.form["timeleft"] = -1
-			execute(
-				"DELETE FROM runtime_data WHERE id = ?",
-				(session["peer_id"], )
-			)
-		else:
-			request.form["timeleft"] = timeleft
-		return request.form
+    if request.method == "GET":
+        peer = query(
+            "SELECT * FROM runtime_data WHERE id = ?",
+            (session["peer_id"],),
+            fetchone=True
+        )
+        if peer == None:
+            return ""
+        _, score, timeleft, currenttime = peer
+        request.form["score"] = score
+        if currenttime + 2 < int(time.time()):
+            # TODO: config time interval
+            request.form["timeleft"] = -1
+            execute(
+                "DELETE FROM runtime_data WHERE id = ?",
+                (session["peer_id"], )
+            )
+        else:
+            request.form["timeleft"] = timeleft
+        return request.form
